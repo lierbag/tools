f9cb65abcb4bfd4388e133bd73c4ccd818cbe709
lierbag <yaojunjunde@163.com>
2017-01-12 15:58:49 +0800

    handle static req

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 
 ## http server
 使用ThreadingMixIn类和HTTPServer类实现了一个非常简单的多线程server
-- 启动方法如demo.py中所示
+- server使用方法如demo.py中所示, 启动: ./demo.py 2>/dev/null &
 - server预处理了get和post参数，具体业务逻辑可在__do_get()和__do_post()方法中实现
 - __do_get(self, get)中get参数是dict类型，包含了所有的get参数
 - __do_post(self,post)中post参数是字符串类型，包含post请求的包体数据
 - 调用__send_response(self, data)返回数据给客户端，其中data是返回结果，字符串类型
 - 日志处理也比较简单，直接输入在当前目录下run.log中
+- 静态文件放在static目录下，请求静态文件时uri path必须以static开头. eq: hostname:port/static/xxx.html 则会将static/xxx.html返回
 
 几个要注意的地方：
 + get参数处理不够健壮，如果get参数不符合规范，处理可能出错
--- httpserver/server.py
+++ httpserver/server.py
@@ -20,7 +20,10 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         try:
             if not self.__parse_get_req():
                 logging.warning("parse get request failed")
-            self.__do_get(self.get)
+            if self.static:
+                self.__handle_static()
+            else:
+                self.__do_get(self.get)
         except Exception as e:
             logging.warning("handle get request error: %s" % str(e))
             self.__send_response("service error")
@@ -44,9 +47,14 @@ class MyHttpHandler(BaseHTTPRequestHandler):
     def __parse_get_req(self):
         # deal get params
         self.get = {}
+        self.static = False
         ul = self.path.split('?')
         if len(ul) < 2:
             logging.info("no get params")
+            paths = ul[0].split('/')
+            if paths[1] == 'static':
+                self.static = True
+                self.static_path = ul[0].strip('/')
         else:
             pl = ul[1].split('&')
             if len(pl) < 2:
@@ -66,6 +74,15 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         # deal post parsms
         self.post = self.rfile.read(int(length))
         return True
+
+    def __handle_static(self):
+        if not os.path.exists(self.static_path):
+            logging.warning("no such file: %s" % self.static_path)
+            self.__send_response("not found")
+        else:
+            with open (self.static_path) as f:
+                data = f.read()
+            self.__send_response(data)
 
     def __do_get(self, get):
         # todo
